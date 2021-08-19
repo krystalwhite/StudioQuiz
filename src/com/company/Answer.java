3c080c1a8901fc0911bea011f47a33f4b8445ef6
krystalwhite <krystalsnowhite@gmail.com>
2021-08-18 20:52:42 -0500

    starting ideas

---
+++ src/com/company/Answer.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Answer {
+}
+
+
+//arraylist --> checkbox (as multiple options), true/false (has 2 options)
---
+++ src/com/company/CheckBox.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class CheckBox extends Question {
+
+    public CheckBox (Question question, Answer answer) {
+        super();
+
+    }
+}
